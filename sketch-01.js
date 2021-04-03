c536fee92c889f46b18c59d63eb59e6488e45123
Ellen-enn <78328252+Ellen-enn@users.noreply.github.com>
2021-04-03 09:23:22 +0800

    Add files via upload

---
+++ sketch-01.js
@@ -0,0 +1,279 @@
+function setup() {
+
+    createCanvas(1000,1000);
+    background(0);
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 10, 20, 20); 
+        i = i + 1;
+    }
+    
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 30, 20, 20); 
+        i = i + 1;
+    }
+    
+    
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 50, 20, 20); 
+        i = i + 1;
+    }
+   
+   
+   
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 70, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 90, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 110, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 130, 20, 20); 
+        i = i + 1;
+    }
+    
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 150, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 400, 20, 20); 
+        i = i + 1;
+    }
+    
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 420, 20, 20); 
+        i = i + 1;
+    }
+
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 440, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 460, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 480, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 180) {
+        ellipse(5 * i, 500, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(10, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(30, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(50, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(70, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(90, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(110, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+    
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(130, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+    
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(150, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+    
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(170, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+    
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(190, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+    var i; 
+    i = 0;
+    
+    stroke(random(255),random(255),random(255));
+    fill(random(255),random(255),random(255));
+    while (i < 210) {
+        ellipse(210, 5 * i, 20, 20); 
+        i = i + 1;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function mousePressed() {
+
+    saveCanvas("sketch-01","png") 
+}
